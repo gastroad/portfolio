3536f6962c6adce6c48275a5724445cb27b5d49c
진현성 <gostroad37@gmail.com>
2023-02-15 05:54:29 +0900

    mockData 수정

--- src/api/mockData.ts
+++ src/api/mockData.ts
@@ -2,7 +2,7 @@ const portfolioCardData = [
     {
         img: require("../assets/image/profile.jpeg").default,
         title: "이력서",
-        paragraph: "5년차 프론트엔드 개발자 진현성 이력서 입니다.",
+        paragraph: "프론트엔드 개발자 진현성 이력서 입니다.",
         link: "https://gastroad.github.io/resume/"
     }, {
         img: require("../assets/image/github.png").default,
