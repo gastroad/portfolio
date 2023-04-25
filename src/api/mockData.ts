ed522b74e2ad890f295db41a9bc893db800afbdf
진현성 <gostroad37@gmail.com>
2023-04-25 19:11:27 +0900

    포트폴리오에 깃블로그 추가

--- src/api/mockData.ts
+++ src/api/mockData.ts
@@ -10,6 +10,12 @@ const portfolioCardData = [
         paragraph: "github 링크 입니다.",
         link: "https://github.com/gastroad"
     }, {
+    }, {
+        img: require("../assets/image/github.png").default,
+        title: "GitHub",
+        paragraph: "개인 blog 입니다.",
+        link: "https://gastroad.github.io"
+    }, {
         img: require("../assets/image/slides.png").default,
         title: "Slides",
         paragraph: "pt발표 자료 입니다.",
